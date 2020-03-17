706b1ac08413323339eed40d0655aac2712f0180
Nazia Shaik <shaiknazia.n@gmail.com>
2020-03-17 20:15:20 +0000

    added header

--- express_server.js
+++ express_server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; //default port 8080
+const PORT = 8080;
+
+app.set("view engine", "ejs"); //Added ejs
+
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -15,6 +19,22 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls : urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  let templateVars = { shortURL, longURL: urlDatabase[shortURL]};
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
 });
+
